8a15c27d8c4f07e249180f70c18d987a50eb938b
bhavyakarimikonda <bhavyakarimikonda@172.20.10.3>
2023-05-26 02:18:40 +0530

    Initial commit

---
+++ automate_onboarding_usingsdk/src/test/java/automate_onboarding/BaseClass2.java
@@ -0,0 +1,72 @@
+package automate_onboarding;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+// Running without Automate
+
+public class BaseClass2{
+	
+	public WebDriver driver;
+
+	@BeforeSuite
+    public void setUp() throws Exception {
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("start-maximized");
+        driver = new FirefoxDriver(options);
+        driver.manage().window().maximize();
+        driver.get("https://www.browserstack.com/users/sign_in");
+       // Thread.sleep(25000);
+    }
+   
+	
+    @Test(priority=1)
+    public void LogintoPortal() throws InterruptedException {
+    	// driver.findElement(By.id("user_email_login")).sendKeys(Keys.CONTROL + "a");
+        //driver.findElement(By.id("user_password")).sendKeys(Keys.CONTROL + "a");
+    //	 driver.findElement(By.id("user_email_login")).clear();
+    //	 driver.findElement(By.id("user_password")).clear();
+    	  Thread.sleep(1000);
+        driver.findElement(By.id("user_email_login")).sendKeys(System.getenv("browserstack_username"));
+        driver.findElement(By.id("user_password")).sendKeys(System.getenv("browserstack_password"));
+        driver.findElement(By.id("user_submit")).click();
+        Thread.sleep(1000);
+        String loginSuccessMsg = "You are already part of  team";
+        String checkLoginSuccess = driver.findElement(By.xpath("/html/body/main/div[2]/div[3]/section/h3")).getText();
+        assert loginSuccessMsg.equals(checkLoginSuccess);
+    }
+    
+	
+	  @Test(priority=2) public void LaunchLive() throws InterruptedException {
+		  Thread.sleep(1000); 
+		  driver.get("https://live.browserstack.com/dashboard");
+		  Thread.sleep(1000); 
+	  }
+	 
+    
+    @Test(priority=3)
+    public void LaunchBrowser() throws InterruptedException {
+    	   // 	driver.findElement(By.xpath("/html/body/main/div/div[21]/div/div[2]/div/div[2]/div[2]/div[1]/div[1]/div/div/span")).click() ;
+    		driver.findElement(By.xpath("//*[@id=\"platform-list-react\"]/div/div[2]/div/div[2]/div[3]/div[1]/div[1]/div/div")).click();
+    		Thread.sleep(5000);
+    }
+    
+
+
+	@AfterSuite
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+}
